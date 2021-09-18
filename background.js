54652f953dffd1bd758ace40b801c34772c97e16
chrisfku <chrisfku@student.matnat.uio.no>
2021-09-18 19:56:32 +0200

    Refactored background.js

--- background.js
+++ background.js
@@ -1,55 +1,23 @@
-let defaultVolume = 20;
-let defaultIncrement = 5;
-let useDefaultVolume = false;
-let useMousewheelVolume = true;
-let defaultColor = "#ffffff"; //white
-let defaultFontSize = 40;
-let useOverlayMouse = true;
-let defaultModifierKey = "Shift";
-let useModifierKey = false;
-let defaultBlacklist = [
-  "music.youtube.com",
-  "open.spotify.com"
-];
+importScripts('config.js')
 
+const setDefaultSetting = (setting, defaultValue) => {
+  chrome.storage.sync.get(setting, (data) => {
+    if (data[setting] === undefined) {
+      chrome.storage.sync.set({[setting]: defaultValue});
+    }
+  });
+};
+
+// Load initial settings from config
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.get({volume: defaultVolume}, (data) => {
-    chrome.storage.sync.set({volume: data.volume});
-  });
-
-  chrome.storage.sync.get({increment: defaultIncrement}, (data) => {
-    chrome.storage.sync.set({increment: data.increment});
-  });
-
-  chrome.storage.sync.get({useDefaultVolume: useDefaultVolume}, (data) => {
-    chrome.storage.sync.set({useDefaultVolume: data.useDefaultVolume});
-  });
-
-  chrome.storage.sync.get({useMousewheelVolume: useMousewheelVolume}, (data) => {
-    chrome.storage.sync.set({useMousewheelVolume: data.useMousewheelVolume});
-  });
-
-  chrome.storage.sync.get({fontColor: defaultColor}, (data) => {
-    chrome.storage.sync.set({fontColor: data.fontColor});
-  });
-
-  chrome.storage.sync.get({fontSize: defaultFontSize}, (data) => {
-    chrome.storage.sync.set({fontSize: data.fontSize});
-  });
-
-  chrome.storage.sync.get({useOverlayMouse: useOverlayMouse}, (data) => {
-    chrome.storage.sync.set({useOverlayMouse: data.useOverlayMouse});
-  });
-
-  chrome.storage.sync.get({useModifierKey: useModifierKey}, (data) => {
-    chrome.storage.sync.set({useModifierKey: data.useModifierKey});
-  });
-
-  chrome.storage.sync.get({modifierKey: defaultModifierKey}, (data) => {
-    chrome.storage.sync.set({modifierKey: data.modifierKey});
-  });
-
-  chrome.storage.sync.get({blacklist: defaultBlacklist}, (data) => {
-    chrome.storage.sync.set({blacklist: data.blacklist});
-  });
+  setDefaultSetting('volume', config.defaultVolume);
+  setDefaultSetting('increment',config.defaultIncrement);
+  setDefaultSetting('useDefaultVolume', config.useDefaultVolume);
+  setDefaultSetting('useMousewheelVolume', config.useMousewheelVolume);
+  setDefaultSetting('fontColor', config.defaultColor);
+  setDefaultSetting('fontSize', config.defaultFontSize);
+  setDefaultSetting('useOverlayMouse', config.useOverlayMouse);
+  setDefaultSetting('useModifierKey', config.useModifierKey);
+  setDefaultSetting('modifierKey', config.defaultModifierKey);
+  setDefaultSetting('blacklist', config.defaultBlacklist);
 });
---
+++ config.js
@@ -0,0 +1,15 @@
+const config = {
+    defaultVolume: 20,
+    defaultIncrement: 5,
+    useDefaultVolume: false,
+    useMousewheelVolume: true,
+    defaultColor: "#ffffff", //white
+    defaultFontSize: 40,
+    defaultModifierKey: "Shift",
+    useOverlayMouse: true,
+    useModifierKey: false,
+    defaultBlacklist: [
+        "music.youtube.com",
+        "open.spotify.com"
+    ]
+}
