f51b8de4527aa09c3bab26dd766e03c86f492d8d
Fredrik Wihelmsen <fredrik.wilhelmsen0@gmail.com>
2021-09-25 19:49:49 +0200

    Refactored background.js
    All settings are now stored as one object.

--- background.js
+++ background.js
@@ -1,55 +1,21 @@
-let defaultVolume = 20;
-let defaultIncrement = 5;
-let useDefaultVolume = false;
-let useMousewheelVolume = true;
-let defaultColor = "#ffffff"; //white
-let defaultFontSize = 40;
-let useOverlayMouse = true;
-let defaultModifierKey = "Shift";
-let useModifierKey = false;
-let defaultBlacklist = [
-  "music.youtube.com",
-  "open.spotify.com"
-];
+const defaultSettings {
+  defaultVolume = 20,
+  defaultIncrement = 5,
+  useDefaultVolume = false,
+  useMousewheelVolume = true,
+  defaultColor = "#ffffff", //white
+  defaultFontSize = 40,
+  useOverlayMouse = true,
+  defaultModifierKey = "Shift",
+  useModifierKey = false,
+  defaultBlacklist = [
+    "music.youtube.com",
+    "open.spotify.com"
+  ]
+}
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.get({volume: defaultVolume}, (data) => {
-    chrome.storage.sync.set({volume: data.volume});
-  });
-
-  chrome.storage.sync.get({increment: defaultIncrement}, (data) => {
-    chrome.storage.sync.set({increment: data.increment});
-  });
-
-  chrome.storage.sync.get({useDefaultVolume: useDefaultVolume}, (data) => {
-    chrome.storage.sync.set({useDefaultVolume: data.useDefaultVolume});
-  });
-
-  chrome.storage.sync.get({useMousewheelVolume: useMousewheelVolume}, (data) => {
-    chrome.storage.sync.set({useMousewheelVolume: data.useMousewheelVolume});
-  });
-
-  chrome.storage.sync.get({fontColor: defaultColor}, (data) => {
-    chrome.storage.sync.set({fontColor: data.fontColor});
-  });
-
-  chrome.storage.sync.get({fontSize: defaultFontSize}, (data) => {
-    chrome.storage.sync.set({fontSize: data.fontSize});
-  });
-
-  chrome.storage.sync.get({useOverlayMouse: useOverlayMouse}, (data) => {
-    chrome.storage.sync.set({useOverlayMouse: data.useOverlayMouse});
-  });
-
-  chrome.storage.sync.get({useModifierKey: useModifierKey}, (data) => {
-    chrome.storage.sync.set({useModifierKey: data.useModifierKey});
-  });
-
-  chrome.storage.sync.get({modifierKey: defaultModifierKey}, (data) => {
-    chrome.storage.sync.set({modifierKey: data.modifierKey});
-  });
-
-  chrome.storage.sync.get({blacklist: defaultBlacklist}, (data) => {
-    chrome.storage.sync.set({blacklist: data.blacklist});
+  chrome.storage.sync.get({userSettings: defaultVolume}, result => {
+    chrome.storage.sync.set({userSettings: data.userSettings});
   });
 });
