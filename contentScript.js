bd1ac9eb696ee02651c9919d95f215fffcd2b0da
Fredrik Wihelmsen <fredrik.wilhelmsen0@gmail.com>
2021-09-26 02:33:17 +0200

    More settings will update live
    Modifier key, Blacklist, default volume, and scroll will all update live.

--- contentScript.js
+++ contentScript.js
@@ -1,69 +1,77 @@
 let body = document.documentElement || document.body || document.getElementsByTagName("body")[0];
 let settings = {};
-let isModifierKeyPressed = true;
+let isModifierKeyPressed = false;
 
 let onScroll = function (event) {
-    if (isModifierKeyPressed) {
-        let elements = document.elementsFromPoint(event.clientX, event.clientY)
-        for (element of elements) {
-            if (element.tagName == "VIDEO") {
-                event.preventDefault();
+    if(settings.blacklist.includes(window.location.hostname))
+        return;
 
-                let video = element;
-                let volume = 1;
+    if (!settings.useMousewheelVolume)
+        return;
 
-                if (video.volume > settings.volumeIncrement / 100 || (video.volume == settings.volumeIncrement / 100 && event.deltaY < 0) || !settings.usePreciseScroll) {
-                    volume = video.volume + (settings.volumeIncrement / 100) * (event.deltaY / 100 * -1); //deltaY is how much the wheel scrolled, 100 up, -100 down. Divided by 100 to only get direction, then inverted
+    if(settings.useModifierKey && !isModifierKeyPressed)
+        return;
 
-                    //Rounding the volume to the nearest increment, in case the original volume was not on the increment.
-                    volume = volume * 100;
-                    volume = volume / settings.volumeIncrement;
-                    volume = Math.round(volume);
-                    volume = volume * settings.volumeIncrement;
-                    volume = volume / 100;
-                }
-                else {
-                    volume = video.volume + (1 / 100) * (event.deltaY / 100 * -1);
-                }
+    let elements = document.elementsFromPoint(event.clientX, event.clientY)
+    for (element of elements) {
+        if (element.tagName != "VIDEO")
+            continue;
 
-                //Limiting the volume to between 0-1
-                if (volume < 0) {
-                    volume = 0;
-                } else if (volume > 1) {
-                    volume = 1;
-                }
+        event.preventDefault();
 
-                if (volume > 0) {
-                    video.muted = false;
-                } else {
-                    video.muted = true;
-                }
+        let video = element;
+        let volume = 1;
 
-                video.volume = volume;
-                video.dataset.volume = volume;
+        if (video.volume > settings.volumeIncrement / 100 || (video.volume == settings.volumeIncrement / 100 && event.deltaY < 0) || !settings.usePreciseScroll) {
+            volume = video.volume + (settings.volumeIncrement / 100) * (event.deltaY / 100 * -1); //deltaY is how much the wheel scrolled, 100 up, -100 down. Divided by 100 to only get direction, then inverted
 
-                //Update overlay text
-                let div = document.getElementById("volumeOverlay");
-                div.innerHTML = Math.round(video.volume * 100);
-                div.style.color = settings.fontColor;
-                div.style.fontSize = settings.fontSize + "px";
-
-                //position the overlay
-                if (settings.useOverlayMouse) {
-                    div.style.top = window.scrollY + event.clientY - div.offsetHeight + "px";
-                    div.style.left = window.scrollX + event.clientX - div.offsetWidth + "px";
-                } else {
-                    let vidPos = video.getBoundingClientRect();
-                    div.style.top = 10 + window.scrollY + vidPos.top + "px";
-                    div.style.left = 10 + window.scrollX + vidPos.left + "px";
-                }
-
-                //Animate fade
-                let newDiv = div;
-                div.parentNode.replaceChild(newDiv, div);
-                div.classList.add("scrollOverlayFade");
-            }
+            //Rounding the volume to the nearest increment, in case the original volume was not on the increment.
+            volume = volume * 100;
+            volume = volume / settings.volumeIncrement;
+            volume = Math.round(volume);
+            volume = volume * settings.volumeIncrement;
+            volume = volume / 100;
         }
+        else {
+            volume = video.volume + (1 / 100) * (event.deltaY / 100 * -1);
+        }
+
+        //Limiting the volume to between 0-1
+        if (volume < 0) {
+            volume = 0;
+        } else if (volume > 1) {
+            volume = 1;
+        }
+
+        if (volume > 0) {
+            video.muted = false;
+        } else {
+            video.muted = true;
+        }
+
+        video.volume = volume;
+        video.dataset.volume = volume;
+
+        //Update overlay text
+        let div = document.getElementById("volumeOverlay");
+        div.innerHTML = Math.round(video.volume * 100);
+        div.style.color = settings.fontColor;
+        div.style.fontSize = settings.fontSize + "px";
+
+        //position the overlay
+        if (settings.useOverlayMouse) {
+            div.style.top = window.scrollY + event.clientY - div.offsetHeight + "px";
+            div.style.left = window.scrollX + event.clientX - div.offsetWidth + "px";
+        } else {
+            let vidPos = video.getBoundingClientRect();
+            div.style.top = 10 + window.scrollY + vidPos.top + "px";
+            div.style.left = 10 + window.scrollX + vidPos.left + "px";
+        }
+
+        //Animate fade
+        let newDiv = div;
+        div.parentNode.replaceChild(newDiv, div);
+        div.classList.add("scrollOverlayFade");
     }
 }
 
@@ -81,6 +89,12 @@ let handleDefaultVolume = function (video) {
 };
 
 let setAudio = function (mutations) {
+    if(!settings.useDefaultVolume)
+        return;
+
+    if(settings.blacklist.includes(window.location.hostname))
+        return;
+
     for (mutation of mutations) {
         for (node of mutation.addedNodes) {
             if (node.tagName != "VIDEO")
@@ -88,9 +102,7 @@ let setAudio = function (mutations) {
 
             let video = node;
 
-            if (settings.useDefaultVolume) {
-                handleDefaultVolume(video);
-            }
+            handleDefaultVolume(video);
         }
     }
 }
@@ -102,38 +114,28 @@ chrome.storage.sync.get("userSettings", result => {
         settings = changes.userSettings.newValue;
     })
 
-    if (settings.blacklist.includes(window.location.hostname)) {
-        return;
-    }
+    body.addEventListener("keydown", function (event) {
+        if (settings.modifierKey == event.key) {
+            isModifierKeyPressed = true;
+        }
+    });
 
-    if (settings.useModifierKey) {
-        isModifierKeyPressed = false;
+    body.addEventListener("keyup", function (event) {
+        if (settings.modifierKey == event.key) {
+            isModifierKeyPressed = false;
+        }
+    });
 
-        body.addEventListener("keydown", function (event) {
-            if (settings.modifierKey == event.key) {
-                isModifierKeyPressed = true;
-            }
-        });
+    document.addEventListener("wheel", onScroll, {passive: false});
 
-        body.addEventListener("keyup", function (event) {
-            if (settings.modifierKey == event.key) {
-                isModifierKeyPressed = false;
-            }
-        });
-    }
+    //Add volume overlay to the page
+    let div = document.createElement("div");
+    div.id = "volumeOverlay";
+    div.classList.add("scrollOverlay");
+    div.style.color = settings.fontColor;
+    div.style.fontSize = settings.fontSize + "px";
 
-    if (settings.useMousewheelVolume) {
-        document.addEventListener("wheel", onScroll, {passive: false});
-
-        //Add volume overlay to the page
-        let div = document.createElement("div");
-        div.id = "volumeOverlay";
-        div.classList.add("scrollOverlay");
-        div.style.color = settings.fontColor;
-        div.style.fontSize = settings.fontSize + "px";
-
-        body.appendChild(div);
-    }
+    body.appendChild(div);
 
     const config = {
         childList: true,
