81c5cbc09a6c7151d57bc6fe798cb61bdc417cd2
Fredrik Wihelmsen <fredrik.wilhelmsen0@gmail.com>
2024-08-15 11:55:59 +0200

    Added settings interface

---
+++ src/Settings.tsx
@@ -0,0 +1,23 @@
+export interface Settings {
+    useDefaultVolume: boolean,
+    defaultVolume: number,
+
+    volumeIncrement: number,
+    usePreciseScroll: boolean,
+    useMouseWheelVolume: boolean,
+    useUncappedVolume: boolean,
+    modifierKey: string,
+    useModifierKey: boolean,
+    invertModifierKey: boolean,
+    toggleMuteKey: string,
+    useToggleMuteKey: boolean,
+
+    fontColor: string,
+    fontSize: number,
+    useOverlayMouse: boolean,
+    overlayXPos: number,
+    overlayYPos: number,
+
+    doLogging: boolean,
+    blacklist: string[]
+};
