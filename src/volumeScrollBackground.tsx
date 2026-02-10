4600c76880d89efaf174a67645a20fd9313bfa37
Fredrik Wihelmsen <fredrik.wilhelmsen0@gmail.com>
2026-02-10 21:59:49 +0100

    Reworked onInstalled function to (hopefully) better handle updates.

--- src/volumeScrollBackground.tsx
+++ src/volumeScrollBackground.tsx
@@ -1,9 +1,18 @@
 import browser from "webextension-polyfill";
-import { defaultSettings } from './types';
+import { defaultSettings, Settings } from './types';
 
-browser.runtime.onInstalled.addListener(() => {
-    browser.storage.local.get({settings: defaultSettings})
-        .then((result) => {
-            browser.storage.local.set({settings: result.settings});
-        });
+browser.runtime.onInstalled.addListener(async (details) => {
+    if (details.reason === "install") { // First time install
+        await browser.storage.local.set({ settings: defaultSettings });
+    } else if (details.reason === "update") { // Extension updated
+        const data = await browser.storage.local.get("settings");
+        const oldSettings: Settings | Object = data.settings || {};
+
+        const newSettings: Settings = {
+            ...defaultSettings,      // Start with all new defaults
+            ...oldSettings,          // Overwrite with any existing user settings
+        };
+
+        await browser.storage.local.set({ settings: newSettings });
+    }
 });
