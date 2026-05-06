2f60ee3d71bb1137c7ee5de3926fe7d3657278ef
Fredrik Wihelmsen <fredrik.wilhelmsen0@gmail.com>
2026-05-06 20:38:09 +0200

    Fixed display no longer working

--- src/handlers/YTMusic.tsx
+++ src/handlers/YTMusic.tsx
@@ -10,12 +10,11 @@ export class YTMusicHandler extends DefaultHandler {
     protected getVideo(mouseX: number, mouseY: number, debug: (message: String, extra?: any) => void): videoElements | null {
         const video = document.getElementsByTagName("VIDEO")[0];
         const bar = document.getElementsByTagName("YTMUSIC-PLAYER-BAR")[0];
-        const image = document.getElementById("song-image");
         const player = document.getElementsByTagName("YTMUSIC-PLAYER")[0];
 
         if (bar.matches(":hover") || player.matches(":hover")) {
             const videoGroup: videoElements = {
-                display: image as HTMLBaseElement,
+                display: player as HTMLBaseElement,
                 video: video as HTMLVideoElement
             };
 
