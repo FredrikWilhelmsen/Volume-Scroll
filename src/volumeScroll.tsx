2736c063fc12a71b4f3c5460a37261aa753a9e44
Fredrik Wihelmsen <fredrik.wilhelmsen0@gmail.com>
2026-05-07 23:43:52 +0200

    Replaced old hotkey detection for WheelEvent detection

--- src/volumeScroll.tsx
+++ src/volumeScroll.tsx
@@ -3,8 +3,6 @@ import * as controller from "./VolumeScrollController";
 window.addEventListener("wheel", controller.onScroll, { passive: false, capture: true });
 window.addEventListener("mousedown", controller.onMouseDown, { passive: false, capture: true });
 window.addEventListener("mouseup", controller.onMouseUp, { passive: false, capture: true });
-window.addEventListener("keydown", controller.onKeyDown, { passive: false, capture: true });
-window.addEventListener("keyup", controller.onKeyUp, { capture: true });
 window.addEventListener("mousemove", controller.onMouseMove, { capture: true });
 window.addEventListener("contextmenu", controller.onContextMenu, { capture: true });
 window.addEventListener("auxclick", controller.onAuxClick, { capture: true });
