d9ea520f26d6582a84bac89ae9ada7216da2d0cb
Fredrik Wihelmsen <fredrik.wilhelmsen0@gmail.com>
2026-05-13 19:16:39 +0200

    Added pause state tracking

--- src/types.tsx
+++ src/types.tsx
@@ -104,4 +104,5 @@ export interface logElement {
 export interface VideoState {
     targetVolume: number; // Ratio 0-1 (or > 1 for boost)
     isMuted: boolean; // Tracking if we have forced mute
+    isPaused: boolean; // Tracking if we have forced pause
 }
