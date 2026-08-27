c885c9213d4ccb8a52b9183f7d7b898ad6815aa8
Fredrik Wilhelmsen <fredrik.wilhelmsen0@gmail.com>
2026-08-27 23:17:01 +0200

    Deleted YT Music custom handler

--- src/handlers/YTMusic.ts
+++
@@ -1,6 +0,0 @@
-import { DefaultHandler } from "./Default";
-
-export class YTMusicHandler extends DefaultHandler {
-    protected name: string = "YTMusicHandler";
-    protected domains: string[] = ["music.youtube.com"];
-}
